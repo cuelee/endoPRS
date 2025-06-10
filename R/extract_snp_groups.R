e7e02c66e4b589069ae5982c4f0178823ab01f78
Cue Hyunkyu Lee <cue.h.lee@gmail.com>
2025-06-10 16:36:54 -0400

    Fix: add missing updates to endoPRS

--- R/extract_snp_groups.R
+++ R/extract_snp_groups.R
@@ -13,30 +13,34 @@
 #'
 #' @return A named table. The name corresponds to the SNP and value corresponds to the assigned group (pheno only, endo only, or both). It is used as input in the create_penalty_table function
 
-
-
 ## Function to extract snps associated with pheno, with endo, and with both
 extract_snp_groups = function(pheno_gwas, endo_gwas, map, thresh, filter_hapmap, hapmap){
 
-  ## Clean gwas column names and add unique id
-  pheno_gwas_clean = format_gwas(pheno_gwas, map, filter_hapmap, hapmap)
-  endo_gwas_clean = format_gwas(endo_gwas, map, filter_hapmap, hapmap)
+    ## Clean gwas column names and add unique id
+    pheno_gwas_clean = format_gwas(pheno_gwas, map, filter_hapmap, hapmap)
+    endo_gwas_clean = format_gwas(endo_gwas, map, filter_hapmap, hapmap)
+    
+    ## Filter gwas based on p-value
+    pheno_gwas_thresh = pheno_gwas_clean[pheno_gwas_clean$P < thresh,]
+    endo_gwas_thresh = endo_gwas_clean[endo_gwas_clean$P < thresh,]
+    
+    ## Create extract SNPs below p-value threshold for each trait
+    df_pheno = data.frame(SNP = pheno_gwas_thresh$unique_id, trait = "pheno")
+    df_endo = data.frame(SNP = endo_gwas_thresh$unique_id, trait = "endo")
+    df_combined = rbind(df_pheno, df_endo)
+    
+    ## Create label as to whether SNP is pheno-only, endo-only, or both
+    snps_assoc_tab <- table(df_combined$SNP)
+    snps_assoc <- rep(NA_character_, length(snps_assoc_tab))
+    names(snps_assoc) <- names(snps_assoc_tab)
+    
+    snps_assoc[snps_assoc_tab == 2] <- "both"
+    snps_assoc[snps_assoc_tab == 1 & names(snps_assoc_tab) %in% df_pheno$SNP] <- "pheno_only"
+    snps_assoc[snps_assoc_tab == 1 & names(snps_assoc_tab) %in% df_endo$SNP] <- "endo_only"
+    
+    # Drop any unassigned (shouldn't exist, but just in case)
+    snps_assoc <- snps_assoc[!is.na(snps_assoc)]
+    
+    return(snps_assoc)
 
-  ## Filter gwas based on p-value
-  pheno_gwas_thresh = pheno_gwas_clean[pheno_gwas_clean$P < thresh,]
-  endo_gwas_thresh = endo_gwas_clean[endo_gwas_clean$P < thresh,]
-
-  ## Create extract SNPs below p-value threshold for each trait
-  df_pheno = data.frame(SNP = pheno_gwas_thresh$unique_id, trait = "pheno")
-  df_endo = data.frame(SNP = endo_gwas_thresh$unique_id, trait = "endo")
-  df_combined = rbind(df_pheno, df_endo)
-
-  ## Create label as to whether SNP is pheno-only, endo-only, or both
-  snps_assoc = table(df_combined$SNP)
-  snps_assoc[snps_assoc == 2] = "both"
-  snps_assoc[snps_assoc == 1 & names(snps_assoc) %in% df_pheno$SNP] = "pheno_only"
-  snps_assoc[snps_assoc == 1 & names(snps_assoc) %in% df_endo$SNP] = "endo_only"
-
-  ## Return snps
-  return(snps_assoc)
 }
